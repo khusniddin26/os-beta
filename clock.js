9ec928e7f66a47173e1663d655fcbd34bb7a4c4b
Khusniddin Madaminjonov <88139269+khusniddin26@users.noreply.github.com>
2024-12-05 11:49:01 +0500

    dsaf
    asdf

---
+++ clock.js
@@ -0,0 +1,11 @@
+var span = document.getElementById('span');
+
+function time() {
+  var d = new Date();
+  var m = d.getMinutes();
+  var h = d.getHours();
+  span.textContent = 
+    ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2);
+}
+
+setInterval(time, 1000);
